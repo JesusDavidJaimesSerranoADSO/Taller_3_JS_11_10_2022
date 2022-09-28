8cbc7217eba92113f5cbebd22f2b25d9715c62db
JesúsDavidJaimesSerranoADSO <jdavidjaimess@gmail.com>
2022-09-28 12:21:25 -0500

    Ejercicio 6

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 addEventListener("DOMContentLoaded",()=>{
-    // Escriba un algoritmo que calcule el área de un rectángulo siempre y cuando los lados sean positivos:
-    // área triangulo= lado * lado.
+//     Considere dos variables llamadas temperatura y presión. Escriba una sentencia if-else que muestre
+// en pantalla la palabra Alarma si la variable presión es mayor a 200 o si la variable temperatura es
+// mayor a 100. En caso contrario, se debe mostrar en pantalla la palabra Normal.
 
-    let valor1=Number(prompt("ingrese el valor de base"))
-    let valor2=Number(prompt("ingrese el valor de altura"))
+    let temperatoura=Number(prompt("ingrese el valor de base"))
+    let presión=Number(prompt("ingrese el valor de altura"))
     
-    if(valor1 > 0 %% valor2 > 0 ){
-        let Triangulo = valor1 * valor2 / 2
-        console.log(`El area del Triangulo es  ${Triangulo}`)
+    if(presión > 200){
+        alert(" Alarma la presión es mayor a 200")
+    }else{
+        alert("Normal")
+    }
+
+    if(temperatoura > 100){
+        alert("Alarma la temperatura es mayor a 200")
+    }else{
+        alert("Normal")
     }
 })
