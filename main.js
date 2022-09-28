76d994931b77c79f972567d723da5141852a55ca
JesúsDavidJaimesSerranoADSO <jdavidjaimess@gmail.com>
2022-09-28 11:22:07 -0500

    Ejercicio 2

--- main.js
+++ main.js
@@ -1,9 +1,44 @@
 addEventListener("DOMContentLoaded",()=>{
-    let ingreso = Number(prompt("ingrese un numero"))
 
-    if(ingreso > 0){
-        if(ingreso <= 100){console.log(`su numero es ${ingreso}`)}
-        else{alert(`lo sentimos su numero es mayor que 100`)}
+    let num1 = Number(prompt("ingrese un numero 1"))
+    let num2 = Number(prompt("ingrese un numero 2"))
+    let num3 = Number(prompt("ingrese un numero 3"))
+
+    if(num1 > num2 && num3){
+        if(num2 > num3){
+            console.log(`en munero mayor es ${num1}`)
+            console.log(`en munero segundo es ${num2}`)
+            console.log(`en munero ultimo es ${num3}`)
+        } 
+        else {
+            console.log(`en munero mayor es ${num1}`)
+            console.log(`en munero segundo es ${num3}`)
+            console.log(`en munero ultimo es ${num2}`)
+        }
     }
-    else{alert(`su numero es negativo o cero`)}
+
+    else if(num2 > num1 && num3){
+        if(num1 > num3){
+            console.log(`en munero mayor es ${num2}`)
+            console.log(`en munero segundo es ${num1}`)
+            console.log(`en munero ultimo es ${num3}`)
+        } 
+        else {
+            console.log(`en munero mayor es ${num2}`)
+            console.log(`en munero segundo es ${num3}`)
+            console.log(`en munero ultimo es ${num1}`)
+        }
+    }
+    else if(num3 > num1 && num2){
+        if(num1 > num2){
+            console.log(`en munero mayor es ${num3}`)
+            console.log(`en munero segundo es ${num1}`)
+            console.log(`en munero ultimo es ${num2}`)
+        } 
+        else {
+            console.log(`en munero mayor es ${num3}`)
+            console.log(`en munero segundo es ${num2}`)
+            console.log(`en munero ultimo es ${num1}`)
+        }
+    }
 })
