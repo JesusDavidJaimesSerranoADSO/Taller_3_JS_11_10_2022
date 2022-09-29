9c4a2dae1efd64e1babef9426320aa2122e638a8
JesúsDavidJaimesSerranoADSO <jdavidjaimess@gmail.com>
2022-09-29 11:54:41 -0500

    Ejercicio 12

--- main.js
+++ main.js
@@ -1,25 +1,14 @@
 addEventListener("DOMContentLoaded",()=>{
-    // Programa para solicitar el nombre, apellido, edad y la nota promedio de 5 estudiantes de un curso
-    // de computación.
-    let nombre =(prompt("ingrese su nombre"))
-    let apellido =(prompt("ingrese su apellido"))
-    let edad =(prompt("ingrese su edad"))
+    // Restarle al primer número al segundo (siempre y cuando el primero sea mayor que él segundo, en
+    //     caso contrario indicar con un mensaje que la operación no es posible realizarla.
     let nota1 =Number(prompt("ingrese su nota 1"))
     let nota2 =Number(prompt("ingrese su nota 2"))
-    let nota3 =Number(prompt("ingrese su nota 3"))
-    let nota4 =Number(prompt("ingrese su nota 4"))
-    let nota5 =Number(prompt("ingrese su nota 5"))
 
-    let total = nota1 + nota2 + nota3 + nota4 + nota5
-    let iva = total / 5
-    
-    console.log(` señor /ra ${ nombre}`)
-    console.log(` su promedio es ${iva}`)
-    if(iva < 5){console.log(` usted tienen un promedio bajo`)}
-    
-    else if(iva > 5 && iva < 8){console.log(` usted tienen un promedio alto`)}
-    
-    else{console.log(`usted tienen un promedio superior `)}
+    if(nota1 > nota2){
+        let total = nota1 - nota2
+        console.log(` la resta es ${ total}`)
+    }
+    else{console.log(`la operación no es posible realizarla`)}
 
 
 })
